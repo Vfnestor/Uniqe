74a654a6e9220f44bed53f70ce1b3d90487c804e
Nestor <vahid.fadaei.73@gmail.com>
2026-09-16 13:51:41 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import "./placeholder.css";
-
 import "@/components/ui/ui.css";
+import "@/components/theme/theme.css";
+
+import { ThemeProvider } from "@/components/theme/ThemeProvider";
 
 export const metadata: Metadata = {
   title: {
@@ -45,8 +47,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      data-theme="dark"
+      suppressHydrationWarning
+    >
+      <body>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
