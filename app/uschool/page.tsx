5b56ba36fe2c4e3d932c6f11bd8514783750319d
Nestor <vahid.fadaei.73@gmail.com>
2026-09-16 10:05:58 +0330

    Create app/uschool/page.tsx

---
+++ app/uschool/page.tsx
@@ -0,0 +1,9 @@
+export default function USchoolPage() {
+  return (
+    <main className="placeholder-page">
+      <span>UNIqe</span>
+      <h1>USchool</h1>
+      <p>Learning & education.</p>
+    </main>
+  );
+}
