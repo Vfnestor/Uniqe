f0ce2ec5bf3502689588f2442e1ad4c14475a469
Nestor <vahid.fadaei.73@gmail.com>
2026-09-16 14:33:36 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,15 @@
-import Navbar from "@/components/navigation/Navbar";
 import Hero from "@/components/home/Hero";
 import Ecosystem from "@/components/home/Ecosystem";
 import Manifesto from "@/components/home/Manifesto";
 import Footer from "@/components/home/Footer";
 
-import "@/components/navigation/navbar.css";
-import "@/components/home/hero.css";
-import "@/components/home/ecosystem.css";
-import "@/components/home/manifesto.css";
-import "@/components/home/footer.css";
-
-export default function Home() {
+export default function HomePage() {
   return (
-    <>
-      <Navbar />
-
-      <main className="site-main">
-        <Hero />
-        <Ecosystem />
-        <Manifesto />
-      </main>
-
+    <main>
+      <Hero />
+      <Ecosystem />
+      <Manifesto />
       <Footer />
-    </>
+    </main>
   );
 }
