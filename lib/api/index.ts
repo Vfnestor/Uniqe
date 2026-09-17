b9a4adf9db0250a305f8ba4f05c31aeb6111336c
Nestor <vahid.fadaei.73@gmail.com>
2026-09-17 08:52:11 +0330

    Create lib/api/index.ts

---
+++ lib/api/index.ts
@@ -0,0 +1,18 @@
+export {
+  apiClient,
+  ApiError,
+} from "./client";
+
+export {
+  apiConfig,
+} from "./config";
+
+export {
+  apiEndpoints,
+} from "./endpoints";
+
+export type {
+  ApiResponse,
+  ApiErrorBody,
+  ApiRequestOptions,
+} from "./types";
