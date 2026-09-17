d78631cc52a6654885c3a09da92d2523b1a4f54e
Nestor <vahid.fadaei.73@gmail.com>
2026-09-17 09:29:38 +0330

    Create lib/content/index.ts

---
+++ lib/content/index.ts
@@ -0,0 +1,43 @@
+export {
+  contentTypes,
+} from "./content-types";
+
+export type {
+  ContentType,
+  ContentStatus,
+  ContentMetadata,
+} from "./content-types";
+
+export {
+  contentConfig,
+} from "./content-config";
+
+export type {
+  ContentPage,
+} from "./models/page";
+
+export type {
+  ContentProject,
+} from "./models/project";
+
+export type {
+  ContentUpdate,
+} from "./models/update";
+
+export type {
+  ContentProduct,
+} from "./models/product";
+
+export type {
+  ContentCourse,
+} from "./models/course";
+
+export type {
+  ContentExperiment,
+} from "./models/experiment";
+
+export type {
+  NavigationItem,
+  NavigationGroup,
+  NavigationMenu,
+} from "./models/navigation";
