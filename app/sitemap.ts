541df98b88f482b9173c9d501849699e44372b19
Nestor <vahid.fadaei.73@gmail.com>
2026-09-17 09:37:37 +0330

    Create app/sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,11 @@
+import type {
+  MetadataRoute,
+} from "next";
+
+import {
+  buildSitemap,
+} from "@/lib/seo";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return buildSitemap();
+}
