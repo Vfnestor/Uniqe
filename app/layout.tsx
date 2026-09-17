a0a231bad6e7f406aac3fb9c5228e22edd54ef22
Nestor <vahid.fadaei.73@gmail.com>
2026-09-17 10:14:30 +0330

    Update۲ layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./responsive.css";
 import "./animations.css";
 import "@/components/notifications/notifications.css";
 import "@/components/favorites/favorites.css";
+import "@/components/states/states.css";
 
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import CommandPaletteGlobal from "@/components/command/CommandPaletteGlobal";
