4c51e5cf96a4446a158497d4872c5c708215e776
Nestor <vahid.fadaei.73@gmail.com>
2026-09-17 11:10:29 +0330

    Updateبب layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,21 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./responsive.css";
 import "./animations.css";
+
 import "@/components/notifications/notifications.css";
 import "@/components/favorites/favorites.css";
 import "@/components/states/states.css";
 
+import "@/components/i18n/i18n.css";
+import "@/components/navigation/global-header.css";
+
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
+import {
+  LanguageProvider,
+} from "@/components/i18n/LanguageProvider";
+
+import GlobalHeader from "@/components/navigation/GlobalHeader";
+
 import CommandPaletteGlobal from "@/components/command/CommandPaletteGlobal";
 
 export const metadata: Metadata = {
@@ -22,11 +32,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html
+      lang="fa"
+      dir="rtl"
+      suppressHydrationWarning
+    >
       <body>
         <ThemeProvider>
-          {children}
-          <CommandPaletteGlobal />
+          <LanguageProvider>
+            <GlobalHeader />
+
+            {children}
+
+            <CommandPaletteGlobal />
+          </LanguageProvider>
         </ThemeProvider>
       </body>
     </html>
