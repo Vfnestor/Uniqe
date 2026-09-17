e5c4cdb5d8e0ba1ee8918524afa79edd14c96125
Nestor <vahid.fadaei.73@gmail.com>
2026-09-17 09:36:05 +0330

    Create lib/seo/index.ts

---
+++ lib/seo/index.ts
@@ -0,0 +1,21 @@
+export {
+  seoConfig,
+} from "./config";
+
+export {
+  buildMetadata,
+} from "./metadata";
+
+export {
+  buildRobots,
+} from "./robots";
+
+export {
+  buildSitemap,
+  publicRoutes,
+} from "./sitemap";
+
+export type {
+  SeoImage,
+  SeoMetadata,
+} from "./types";
