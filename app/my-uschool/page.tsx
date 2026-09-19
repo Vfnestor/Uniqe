6657f471bb2155f48d24b7d53ac16e1f72c849ea
Nestor <vahid.fadaei.73@gmail.com>
2026-09-19 11:13:46 +0330

    Create app/my-uschool/page.tsx

---
+++ app/my-uschool/page.tsx
@@ -0,0 +1,11 @@
+import "@/components/uschool/my-uschool.css";
+
+import MyUSchool from "@/components/uschool/MyUSchool";
+
+export default function MyUSchoolPage() {
+  return (
+    <main className="my-uschool-page">
+      <MyUSchool />
+    </main>
+  );
+}
