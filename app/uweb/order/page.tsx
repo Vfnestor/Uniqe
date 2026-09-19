8162c7d54478bbed0680378db8dccbd1cc5441fa
Nestor <vahid.fadaei.73@gmail.com>
2026-09-19 14:50:43 +0330

    Create app/uweb/order/page.tsx

---
+++ app/uweb/order/page.tsx
@@ -0,0 +1,11 @@
+import "@/components/uweb/uweb.css";
+
+import UWebOrderWizard from "@/components/uweb/UWebOrderWizard";
+
+export default function UWebOrderPage() {
+  return (
+    <main className="uweb-page">
+      <UWebOrderWizard />
+    </main>
+  );
+}
