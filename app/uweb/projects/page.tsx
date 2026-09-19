2a2288ca1debd22bc10036c52f98dd641976898a
Nestor <vahid.fadaei.73@gmail.com>
2026-09-19 15:14:11 +0330

    Create app/uweb/projects/page.tsx

---
+++ app/uweb/projects/page.tsx
@@ -0,0 +1,11 @@
+import "@/components/uweb/uweb.css";
+
+import UWebProjects from "@/components/uweb/UWebProjects";
+
+export default function UWebProjectsPage() {
+  return (
+    <main className="uweb-page">
+      <UWebProjects />
+    </main>
+  );
+}
