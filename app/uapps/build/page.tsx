d621afae913734759f9f2b8829ef433b03c06e68
Nestor <vahid.fadaei.73@gmail.com>
2026-09-20 16:20:14 +0330

    Create app/uapps/build/page.tsx

---
+++ app/uapps/build/page.tsx
@@ -0,0 +1,7 @@
+import "@/components/uapps/uapps.css";
+
+import UAppsBuildForm from "@/components/uapps/UAppsBuildForm";
+
+export default function UAppsBuildPage() {
+  return <UAppsBuildForm />;
+}
