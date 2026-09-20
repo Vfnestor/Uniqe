d622d772f19d1837506cc82deb510482eb9bb37c
Nestor <vahid.fadaei.73@gmail.com>
2026-09-20 12:39:50 +0330

    Update page.tsx

--- app/uapps/page.tsx
+++ app/uapps/page.tsx
@@ -1,19 +1,7 @@
 import "@/components/uapps/uapps.css";
 
-import UAppsHero from "@/components/uapps/UAppsHero";
-import AppsGrid from "@/components/uapps/AppsGrid";
-import UAppsCTA from "@/components/uapps/UAppsCTA";
-
-import { uapps } from "@/components/uapps/apps";
+import UAppsHome from "@/components/uapps/UAppsHome";
 
 export default function UAppsPage() {
-  return (
-    <main className="uapps-page">
-      <UAppsHero />
-
-      <AppsGrid apps={uapps} />
-
-      <UAppsCTA />
-    </main>
-  );
+  return <UAppsHome />;
 }
