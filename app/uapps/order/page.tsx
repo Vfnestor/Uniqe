1bbb6e648daea3d52ea0d64d473d812e40e39399
Nestor <vahid.fadaei.73@gmail.com>
2026-09-20 16:46:39 +0330

    Create app/uapps/order/page.tsx

---
+++ app/uapps/order/page.tsx
@@ -0,0 +1,7 @@
+import "@/components/uapps/uapps.css";
+
+import UAppsOrderForm from "@/components/uapps/UAppsOrderForm";
+
+export default function UAppsOrderPage() {
+  return <UAppsOrderForm />;
+}
